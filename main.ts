6a270463caf0a7be9732e5960ad44d69f6ebc61a
frankmicrobit <100852447+frankmicrobit@users.noreply.github.com>
2025-06-12 13:50:43 +0200

    Første utgave

--- main.ts
+++ main.ts
@@ -1,7 +1,5 @@
-let Vanalog = 0
 let k = 0
 input.onButtonPressed(Button.A, function () {
-    Vanalog = 3.2
-    k = 1023 / Vanalog
-    basic.showNumber(pins.analogReadPin(AnalogReadWritePin.P0) / 310)
+    k = 1024 / 3
+    basic.showNumber(pins.analogReadPin(AnalogReadWritePin.P0) / k)
 })
