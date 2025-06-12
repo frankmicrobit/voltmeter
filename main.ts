aca0e7df7ed9a75b3431e240b7a71345417c76d8
frankmicrobit <100852447+frankmicrobit@users.noreply.github.com>
2025-06-13 00:02:43 +0200

    Kalibrert måling

--- main.ts
+++ main.ts
@@ -1,5 +1,18 @@
-let k = 0
 input.onButtonPressed(Button.A, function () {
-    k = 1024 / 3
-    basic.showNumber(pins.analogReadPin(AnalogReadWritePin.P0) / k)
+    for (let index = 0; index < 48; index++) {
+        k = 1024 / 3
+        radio.sendNumber(pins.analogReadPin(AnalogReadWritePin.P0) / k * 5 + 0.98)
+        basic.pause(50)
+    }
 })
+input.onButtonPressed(Button.B, function () {
+    I = 11
+    for (let index = 0; index < 48; index++) {
+        radio.sendNumber(I)
+        I += 0.05
+        basic.pause(50)
+    }
+})
+let I = 0
+let k = 0
+radio.setGroup(100)
